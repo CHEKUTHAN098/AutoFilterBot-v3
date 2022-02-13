9c967b6b959d898f4644bbe7a581a37e4609c5bd
CHEKUTHAN098 <98800108+CHEKUTHAN098@users.noreply.github.com>
2022-02-13 08:12:06 +0530

    Update Inline.py

--- LuciferMoringstar_Robot/Filter/Inline.py
+++ LuciferMoringstar_Robot/Filter/Inline.py
@@ -89,7 +89,7 @@ async def answer(bot, query):
 
 def get_reply_markup(query):
     buttons = [[
-        InlineKeyboardButton('Deploy Video', url=f'{TUTORIAL}')
+        InlineKeyboardButton('Owner', https://t.me/Lion_098765=f'{Owner}')
         ],[
         InlineKeyboardButton('🔍 Search again 🔎', switch_inline_query_current_chat=query)
         ]]
